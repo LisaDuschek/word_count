5161a69854d2e1496f0cf0ce8da4ec5b97a3db0c
Lisa Duschek <lisa.duschek@hotmail.com>
2015-08-07 11:46:31 -0700

    it should returns number '1' for the word 'awesome' in the sentence 'i wish every exercise was this awesome'

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,4 +6,12 @@ describe ("String#word_count") do
     expect(("I have one cat only").word_count ("cat")).to(eq(1))
   end
 
+  it("returns number '2' for the word 'cat' in the sentence 'I have one cat only,but I want another cat badly'") do
+    expect(("I have one cat only,but I want another cat badly").word_count ("cat")).to(eq(2))
+  end
+
+  it("returns number '1' for the word 'awesome' in the sentence 'i wish every exercise was this awesome'") do
+    expect(("i wish every exercise was this awesome").word_count ("awesome")).to(eq(1))
+  end
+
 end
